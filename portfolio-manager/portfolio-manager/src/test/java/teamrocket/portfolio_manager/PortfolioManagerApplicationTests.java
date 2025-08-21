dc079dfc101725a35299ba13ba2c7bb4a9f36469
Kasia <email@email.com>
2025-08-21 13:44:46 +0000

    initial commit

---
+++ portfolio-manager/portfolio-manager/src/test/java/teamrocket/portfolio_manager/PortfolioManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package teamrocket.portfolio_manager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PortfolioManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
