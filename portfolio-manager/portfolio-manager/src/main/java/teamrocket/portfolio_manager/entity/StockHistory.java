f1166bd191bc5109b14d88d4fb71e25bd4bc5437
Kasia <email@email.com>
2025-08-21 14:21:07 +0000

    StockHistory and StockTransaction without relations

--- portfolio-manager/portfolio-manager/src/main/java/teamrocket/portfolio_manager/entity/StockHistory.java
+++ portfolio-manager/portfolio-manager/src/main/java/teamrocket/portfolio_manager/entity/StockHistory.java
@@ -1,4 +1,19 @@
 package teamrocket.portfolio_manager.entity;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+
+import java.math.BigDecimal;
+import java.util.Date;
+
+@Entity
 public class StockHistory {
+    @Id
+    @GeneratedValue
+    int id;
+    Date date;
+    BigDecimal price;
+
+
 }
--- portfolio-manager/portfolio-manager/src/main/java/teamrocket/portfolio_manager/entity/StockTransaction.java
+++ portfolio-manager/portfolio-manager/src/main/java/teamrocket/portfolio_manager/entity/StockTransaction.java
@@ -1,5 +1,18 @@
 package teamrocket.portfolio_manager.entity;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import org.hibernate.annotations.CollectionIdJdbcTypeCode;
 
+import java.math.BigDecimal;
+import java.util.Date;
+
+@Entity
 public class StockTransaction {
+    @Id
+    @GeneratedValue
+    int id;
+    Date date;
+    BigDecimal amount;
 }
