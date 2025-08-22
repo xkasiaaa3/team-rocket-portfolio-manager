a06eff71ec2a9973905958fd28f46e9392bf63b1
Daniel Ojeda Chen <daniel.oje.che@gmail.com>
2025-08-22 07:32:31 +0000

    Added most basic repository methods

--- portfolio-manager/portfolio-manager/src/main/java/teamrocket/portfolio_manager/repository/StockHistoryRepository.java
+++ portfolio-manager/portfolio-manager/src/main/java/teamrocket/portfolio_manager/repository/StockHistoryRepository.java
@@ -5,7 +5,11 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import teamrocket.portfolio_manager.entity.Stock;
 import teamrocket.portfolio_manager.entity.StockHistory;
+import teamrocket.portfolio_manager.entity.StockTransaction;
+
+import java.util.List;
 
 @Repository
 public interface StockHistoryRepository extends JpaRepository<StockHistory, Integer> {
+    List<StockHistory> findAllByStockId(Integer stock_id);
 }
--- portfolio-manager/portfolio-manager/src/main/java/teamrocket/portfolio_manager/repository/StockTransactionRepository.java
+++ portfolio-manager/portfolio-manager/src/main/java/teamrocket/portfolio_manager/repository/StockTransactionRepository.java
@@ -6,6 +6,9 @@ import org.springframework.stereotype.Repository;
 import teamrocket.portfolio_manager.entity.Stock;
 import teamrocket.portfolio_manager.entity.StockTransaction;
 
+import java.util.List;
+
 @Repository
 public interface StockTransactionRepository extends JpaRepository<StockTransaction, Integer> {
+    List<StockTransaction> findAllByStockId(Integer stock_id);
 }
