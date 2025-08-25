669943f2d02f751e8b9283a5a83f0a96265567de
Kasia <email@email.com>
2025-08-25 07:53:10 +0000

    add method findByStockIdAndDate

--- portfolio-manager/portfolio-manager/src/main/java/teamrocket/portfolio_manager/repository/StockHistoryRepository.java
+++ portfolio-manager/portfolio-manager/src/main/java/teamrocket/portfolio_manager/repository/StockHistoryRepository.java
@@ -3,13 +3,13 @@ package teamrocket.portfolio_manager.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-import teamrocket.portfolio_manager.entity.Stock;
 import teamrocket.portfolio_manager.entity.StockHistory;
-import teamrocket.portfolio_manager.entity.StockTransaction;
 
+import java.util.Date;
 import java.util.List;
 
 @Repository
 public interface StockHistoryRepository extends JpaRepository<StockHistory, Integer> {
     List<StockHistory> findAllByStockId(Integer stock_id);
+    StockHistory findByStockIdAndDate(int id, Date date);
 }
