374fb6e46a1d28c0c769aa4966f63ea111a3de38
Daniel Ojeda Chen <daniel.oje.che@gmail.com>
2025-08-26 07:16:08 +0000

    minor FE changes

---
+++ portfolio-manager/portfolio-manager/src/main/resources/static/scripts/history-script.js
@@ -0,0 +1,8 @@
+const base = "http://localhost:8080"
+
+document.querySelector('.hamburger').addEventListener('click', () => {
+  alert('Open portfolio selector or add new portfolio');
+});
+document.querySelector('.user').addEventListener('click', () => {
+  alert('Open user settings or profile');
+});
