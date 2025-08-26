69e90b3bc762036b4ab3beb5643f649cb0270015
Kasia <email@email.com>
2025-08-26 13:39:59 +0000

    add dto

---
+++ portfolio-manager/portfolio-manager/src/main/java/teamrocket/portfolio_manager/model/StockWithChangeDTO.java
@@ -0,0 +1,48 @@
+package teamrocket.portfolio_manager.model;
+
+import jakarta.persistence.Column;
+import jakarta.validation.constraints.Positive;
+
+import java.math.BigDecimal;
+
+public class StockWithChangeDTO {
+    public StockWithChangeDTO(Integer id, String stockSymbol, String stockName, String currency, BigDecimal currentPrice, double change) {
+        this.id = id;
+        this.stockSymbol = stockSymbol;
+        this.stockName = stockName;
+        this.currency = currency;
+        this.currentPrice = currentPrice;
+        this.change = change;
+    }
+
+    private Integer id;
+    private String stockSymbol;
+    private String stockName;
+    private String currency;
+    private BigDecimal currentPrice;
+    private double change;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public String getStockSymbol() {
+        return stockSymbol;
+    }
+
+    public String getStockName() {
+        return stockName;
+    }
+
+    public String getCurrency() {
+        return currency;
+    }
+
+    public BigDecimal getCurrentPrice() {
+        return currentPrice;
+    }
+
+    public double getChange() {
+        return change;
+    }
+}
